06f9fe80341aa87c6ef65a5aa9a4ca06ee29cf7d
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2024-01-30 21:31:22 -1000

    Implement and Perform Selenium suite.

---
+++ src/test/java/edu/hawaii/ti/iam/groupings/selenium/page/AdminPageManageGroupingsTabTest.java
@@ -0,0 +1,243 @@
+package edu.hawaii.ti.iam.groupings.selenium.page;
+
+import static com.codeborne.selenide.Condition.and;
+import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.disappear;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.webdriver;
+import static com.codeborne.selenide.WebDriverConditions.url;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+
+import static com.codeborne.selenide.Selenide.*;
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.ArrayList;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.codeborne.selenide.WebDriverRunner;
+
+import edu.hawaii.ti.iam.groupings.selenium.core.Property;
+import edu.hawaii.ti.iam.groupings.selenium.core.User;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@SpringBootTest
+public class AdminPageManageGroupingsTabTest {
+
+    private static final Log logger = LogFactory.getLog(HomePageTest.class);
+
+    private final Property property;
+    private WebDriver driver;
+
+    // Constructor.
+    public AdminPageManageGroupingsTabTest(@Autowired Property property) {
+        this.property = property;
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+        WebDriverRunner.setWebDriver(new ChromeDriver());
+        //        WebDriverManager.firefoxdriver().setup();
+        //        WebDriverRunner.setWebDriver(new FirefoxDriver());
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        closeWebDriver();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        open(property.value("app.url.login"));
+        driver = WebDriverRunner.getWebDriver();
+        User user = new User.Builder()
+                .username(property.value("admin.user.username"))
+                .password(property.value("admin.user.password"))
+                .firstname(property.value("admin.user.firstname"))
+                .uhnumber(property.value("admin.user.uhnumber"))
+                .build();
+        assertThat(user.getUsername(), not(equalTo("SET-IN-OVERRIDES")));
+
+        $("#username").val(user.getUsername());
+        $("#password").val(user.getPassword());
+        $x("//*[@id=\"login-form-controls\"]/button").click();
+
+        open(property.value("url.admin"));
+        $x("//*[@id=\"overlay\"]/div/div").shouldBe(disappear, Duration.ofSeconds(80));
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.close();
+    }
+
+    @Test
+    public void filterGroupingsTest() {
+        $x("/html/body/main/div[2]/div[2]/div/div[1]/div[1]/div[2]/div/button").click();
+        $x("/html/body/main/div[2]/div[2]/div/div[1]/div[1]/div[2]/div/ul/li[3]/label").click();
+
+        $x("//*[@id=\"manage-groupings\"]/div[2]/table/thead/tr/th[1]").doubleClick();
+        ArrayList<String> groupingsName = new ArrayList<>();
+        while (true) {
+            groupingsName.addAll($$("#manage-groupings > div.table-responsive > table > tbody > tr >td:nth-child(1)").texts());
+            if ($$(byText("Next")).filterBy(visible).first().parent().parent().is(cssClass("disabled"))) {
+                break;
+            }
+            $$(byText("Next")).filterBy(visible).first().parent().parent().click();
+        }
+        ArrayList<String> tempList = new ArrayList<String>(groupingsName);
+        tempList.sort(String::compareToIgnoreCase);
+        assertEquals(groupingsName, tempList);
+        tempList.clear();
+
+
+        ArrayList<String> groupingsDescription = new ArrayList<>();
+        $x("//*[@id=\"manage-groupings\"]/div[3]/nav/ul/li[1]").click();
+        $x("//*[@id=\"manage-groupings\"]/div[2]/table/thead/tr/th[2]").click();
+        while (true) {
+            groupingsDescription.addAll($$("#manage-groupings > div.table-responsive > table > tbody > tr >td:nth-child(2)").texts());
+            if ($$(byText("Next")).filterBy(visible).first().parent().parent().is(cssClass("disabled"))) {
+                break;
+            }
+            $$(byText("Next")).filterBy(visible).first().parent().parent().click();
+        }
+        tempList = new ArrayList<String>(groupingsDescription);
+        tempList.sort(String::compareToIgnoreCase);
+        assertEquals(groupingsDescription, tempList);
+        tempList.clear();
+
+        ArrayList<String> groupingsPath = new ArrayList<>();
+        $x("//*[@id=\"manage-groupings\"]/div[3]/nav/ul/li[1]").click();
+        $x("//*[@id=\"manage-groupings\"]/div[2]/table/thead/tr/th[3]").doubleClick();
+        while (true) {
+            groupingsPath.addAll($$("#manage-groupings > div.table-responsive > table > tbody > tr >td:nth-child(3)").texts());
+            if ($$(byText("Next")).filterBy(visible).first().parent().parent().is(cssClass("disabled"))) {
+                break;
+            }
+            $$(byText("Next")).filterBy(visible).first().parent().parent().click();
+        }
+        tempList = new ArrayList<String>(groupingsPath);
+        tempList.sort(String::compareToIgnoreCase);
+        assertEquals(groupingsPath, tempList);
+        tempList.clear();
+    }
+
+    @Test
+    public void groupingNameTest() {
+        $x("//*[@id=\"manage-groupings\"]/div[1]/div[2]/input").val(property.value("test.grouping.name"));
+        $x("//*[@id=\"manage-groupings\"]/div[2]/table/thead/tr/th[1]").shouldHave(text(" Grouping Name "));
+        $x("//*[@id=\"manage-groupings\"]/div[2]/table/tbody/tr[1]/td[1]").shouldHave(
+                text(property.value("test.grouping.name")));
+    }
+
+    @Test
+    public void groupingDescriptionTest() {
+        $x("//*[@id=\"manage-groupings\"]/div[1]/div[2]/input").val(property.value("test.grouping.name"));
+        $x("//*[@id=\"manage-groupings\"]/div[1]/div[2]/div/button/i").click();
+        $x("//*[@id=\"manage-groupings\"]/div[1]/div[2]/div/ul/li[1]/label").click();
+        $x("//*[@id=\"manage-groupings\"]/div[2]/table/thead/tr/th[2]").shouldHave(text(" Description"));
+        $x("//*[@id=\"manage-groupings\"]/div[2]/table/tbody/tr[1]/td[2]/div").shouldHave(
+                text(property.value("test.grouping.description")));
+    }
+
+    @Test
+    public void groupingSelectionTest() {
+        $x("//*[@id=\"manage-groupings\"]/div[1]/div[2]/input").val(property.value("test.grouping.name"));
+        $x("//*[@id=\"manage-groupings\"]/div[2]/table/tbody/tr[1]/td[1]").click();
+        $x("//*[@id=\"groupNameCol\"]/h2").shouldHave(text(property.value("test.grouping.name")));
+        $x("//*[@id=\"sel\"]/div/section[1]/div/div[2]/div/p").shouldHave(
+                text("Path: " + property.value("test.grouping.path")));
+        $x("//*[@id=\"sel\"]/div/section[1]/div/div[3]/div/div[1]/p").shouldHave(
+                text("Description: " + property.value("test.grouping.description")));
+    }
+
+    @Test
+    public void groupingPath() {
+        $x("//*[@id=\"manage-groupings\"]/div[1]/div[2]/div/button/i").click();
+        $x("//*[@id=\"manage-groupings\"]/div[1]/div[2]/div/ul/li[2]/label").click();
+        $x("//*[@id=\"manage-groupings\"]/div[2]/table/thead/tr/th[3]").shouldHave(text(" Grouping Path "));
+        String groupingName = property.value("admin.user.username") + "-aux";
+        $("#manage-groupings > div.row.m-auto.pt-3.pb-3 > div.col-lg-3.col-md-4.col-12.p-0.pt-1.d-sm-flex > input").setValue(
+                groupingName);
+        String path = "#tmp\\:" + property.value("admin.user.username") + "\\:" + groupingName;
+        $(path).shouldBe(visible);
+    }
+
+    @Test
+    public void clipboardCopyTest() {
+        $x("//*[@id=\"manage-groupings\"]/div[1]/div[2]/input").val(property.value("test.grouping.name"));
+        $x("//*[@id=\"manage-groupings\"]/div[1]/div[2]/div/button/i").click();
+        $x("//*[@id=\"manage-groupings\"]/div[1]/div[2]/div/ul/li[2]/label").click();
+        $x("//*[@id=\"manage-groupings\"]/div[2]/table/thead/tr/th[3]").shouldHave(text(" Grouping Path "));
+        $x("//*[@id=\"manage-groupings\"]/div[2]/table/tbody/tr/td[3]/form/div/div/button/i").click();
+        $x("/html/body/main/div[2]/div[2]/div/div[1]/div[2]/table/tbody/tr[1]/td[3]/form/div/div/button/i").click();
+        //System.out.println(clipboard().getText());
+        //assertEquals("hawaii.edu:custom:test:awy:awy-test", clipboard().getText());
+        String clipboardValue = getClipboardContent();
+        assertEquals(clipboardValue, "get");
+    }
+
+    @Test
+    public void tableSettingTest() {
+        $("#manage-groupings > div.table-responsive > table > thead > tr > th:nth-child(3)").shouldNotBe(visible);
+        $("#manage-groupings > div.table-responsive > table > tbody > tr:nth-child(1) > td.mw-0.p-10.align-middle.d-none.d-sm-table-cell.col-auto > div").shouldBe(visible);
+        $("#manage-groupings > div.row.m-auto.pt-3.pb-3 > div.col-lg-3.col-md-4.col-12.p-0.pt-1.d-sm-flex > div > button").click();
+        $("#manage-groupings > div.row.m-auto.pt-3.pb-3 > div.col-lg-3.col-md-4.col-12.p-0.pt-1.d-sm-flex > div > ul").shouldBe(visible);
+        $(byText("Show Grouping Path")).click();
+        $("#manage-groupings > div.table-responsive > table > thead > tr > th:nth-child(3)").shouldBe(and("Visible and text", visible, text("Grouping Path")));
+        $("#manage-groupings > div.table-responsive > table > thead > tr > th:nth-child(2)").shouldNotBe(visible);
+        $("#hawaii\\.edu\\:custom\\:test\\:listserv-tests\\:JTTEST-L").shouldBe(visible);
+        $("#manage-groupings > div.row.m-auto.pt-3.pb-3 > div.col-lg-3.col-md-4.col-12.p-0.pt-1.d-sm-flex > div > button").click();
+        $(byText("Show All")).click();
+        $("#manage-groupings > div.table-responsive > table > thead > tr > th:nth-child(2)").shouldBe(and("Is visible with text", visible, text("Description")));
+        $("#manage-groupings > div.table-responsive > table > thead > tr > th:nth-child(3)").shouldBe(text("Grouping Path"));
+        $("#manage-groupings > div.table-responsive > table > tbody > tr:nth-child(1) > td:nth-child(3)").should(visible);
+        $("#manage-groupings > div.table-responsive > table > tbody > tr:nth-child(1) > td.mw-0.p-10.align-middle.d-none.d-sm-table-cell.w-35 > div").should(visible);
+    }
+    public static String getClipboardContent() {
+        String clipboardContent = null;
+        try {
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clipboardContent = (String) clipboard.getData(DataFlavor.stringFlavor);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return clipboardContent;
+    }
+}
--- src/test/java/edu/hawaii/ti/iam/groupings/selenium/page/LoggedInAdminTest.java
+++ src/test/java/edu/hawaii/ti/iam/groupings/selenium/page/LoggedInAdminTest.java
@@ -3,7 +3,6 @@ package edu.hawaii.ti.iam.groupings.selenium.page;
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
@@ -23,6 +22,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -39,12 +39,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.conditions.Text;
 
 import edu.hawaii.ti.iam.groupings.selenium.core.Property;
 import edu.hawaii.ti.iam.groupings.selenium.core.User;
 
 @SpringBootTest
-public class HomePageTest {
+public class LoggedInAdminTest {
 
     private static final Log logger = LogFactory.getLog(HomePageTest.class);
 
@@ -52,7 +53,7 @@ public class HomePageTest {
     private WebDriver driver;
 
     // Constructor.
-    public HomePageTest(@Autowired Property property) {
+    public LoggedInAdminTest(@Autowired Property property) {
         this.property = property;
     }
 
@@ -60,6 +61,8 @@ public class HomePageTest {
     public static void beforeAll() {
         WebDriverManager.chromedriver().setup();
         WebDriverRunner.setWebDriver(new ChromeDriver());
+        //        WebDriverManager.firefoxdriver().setup();
+        //        WebDriverRunner.setWebDriver(new FirefoxDriver());
     }
 
     @AfterAll
@@ -69,8 +72,20 @@ public class HomePageTest {
 
     @BeforeEach
     public void setUp() {
-        open(property.value("app.url.home"));
+        open(property.value("app.url.login"));
         driver = WebDriverRunner.getWebDriver();
+        User user = new User.Builder()
+                .username(property.value("admin.user.username"))
+                .password(property.value("admin.user.password"))
+                .firstname(property.value("admin.user.firstname"))
+                .uhnumber(property.value("admin.user.uhnumber"))
+                .build();
+        assertThat(user.getUsername(), not(equalTo("SET-IN-OVERRIDES")));
+
+        $("#username").val(user.getUsername());
+        $("#password").val(user.getPassword());
+        $x("//*[@id=\"login-form-controls\"]/button").click();
+
     }
 
     @AfterEach
@@ -88,27 +103,10 @@ public class HomePageTest {
     }
 
     @Test
-    public void learnMoreButton() {
-        String url = property.value("url.bwiki");
-        $x("//a[@role='button'][@href='" + url + "']").click();
-        webdriver().shouldHave(url("" + url + ""));
-    }
-
-    @Test
-    public void navBarLogin() {
-        String urlRel = property.value("url.relative.login");
-        $x("//a[@href='/" + urlRel + "']").click();
-        $("#uh-seal").shouldBe(visible);
-        String loginUrl = property.value("cas.login.url");
-        String appUrl = property.value("app.url.home");
-        String serviceUrl = appUrl + "/login/cas"; // Why the extra ending?
-        String expectedUrl = loginUrl + "?service=" + encodeUrl(serviceUrl);
-        webdriver().shouldHave(url(expectedUrl));
-    }
-
-    @Test
-    public void loginHereButton() {
-        $x("//button[@class='btn btn-lg dark-teal-bg'][text()='Login Here']").click();
+    public void navBarLogoutButtonText() {
+        $x("/html/body/div/nav/div/div/ul/li[6]/form/button").shouldHave(text("Logout (" + property.value("admin.user.username") + ")"));
+        $x("/html/body/div/nav/div/div/ul/li[6]/form/button").click();
+        $x("/html/body/div/nav/div/div/ul/li[2]/a").shouldHave(text("Login"));
     }
 
     @Test
@@ -125,23 +123,6 @@ public class HomePageTest {
     }
 
     @Test
-    public void loggingInWithStudent() {
-        User user = new User.Builder()
-                .username(property.value("student.user.username"))
-                .password(property.value("student.user.password"))
-                .firstname(property.value("student.user.firstname"))
-                .uhnumber(property.value("student.user.uhnumber"))
-                .build();
-        assertThat(user.getUsername(), not(equalTo("SET-IN-OVERRIDES")));
-
-        navBarLogin();
-
-        $("#username").val(user.getUsername());
-        $("#password").val(user.getPassword());
-        $(by("id", "login-form-controls")).click();
-    }
-
-    @Test
     public void usagePolicyTest() throws Exception {
         File downloadedFile = $("a[href*='ep2.210.pdf']").download();
         Path path = Paths.get(downloadedFile.getPath());
@@ -154,6 +135,68 @@ public class HomePageTest {
         WebElement field = driver.findElement(By.linkText("Usage Policy"));
         assertThat(field.getText(), equalTo("Usage Policy"));
         assertThat(field.getAttribute("href"), endsWith("ep2.210.pdf"));
+    }
+
+    @Test
+    public void navbarAdminTest() {
+        $x("//*[@id=\"navbarSupportedContent\"]/ul/li[1]/a").click();
+        webdriver().shouldHave(url(property.value("url.admin")));
+    }
+
+    @Test
+    public void navbarMembershipsTest() {
+        $x("//*[@id=\"navbarSupportedContent\"]/ul/li[2]/a").click();
+        webdriver().shouldHave(url(property.value("url.memberships")));
+    }
+
+    @Test
+    public void navbarGroupingsTest() {
+        $x("//*[@id=\"navbarSupportedContent\"]/ul/li[3]/a").click();
+        webdriver().shouldHave(url(property.value("url.groupings")));
+    }
+
+    @Test
+    public void navbarAboutTest() {
+        $x("//*[@id=\"navbarSupportedContent\"]/ul/li[4]/a").click();
+        webdriver().shouldHave(url(property.value("url.about")));
+
+    }
+
+    @Test
+    public void navbarFeedbackTest() {
+        $x("//*[@id=\"navbarSupportedContent\"]/ul/li[5]/a").click();
+        webdriver().shouldHave(url(property.value("url.feedback")));
+    }
+
+    @Test
+    public void AdminButtonTest() {
+        $x("/html/body/main/div[3]/div[2]/div/div/div[1]/div[2]/a").click();
+        webdriver().shouldHave(url(property.value("url.admin")));
+    }
+
+    @Test
+    public void membershipsButtonTest() {
+        $x("/html/body/main/div[3]/div[2]/div/div/div[2]/div[2]/a").click();
+        webdriver().shouldHave(url(property.value("url.memberships")));
+    }
+
+    @Test
+    public void groupingsButtonTest() {
+        $x("/html/body/main/div[3]/div[2]/div/div/div[3]/div[2]/a").click();
+        webdriver().shouldHave(url(property.value("url.groupings")));
+    }
+
+    @Test
+    public void welcomeMessageTest() {
+        $x("/html/body/main/div[3]/div[1]/div/div/div[2]/h1").shouldHave(text("Welcome, " + property.value("admin.user.firstname") +"!" ));
+        $x("/html/body/main/div[3]/div[1]/div/div/div[2]/div/h1").shouldHave(text("Role: Admin"));
+    }
+
+    @Test
+    public void announcementTest() {
+        $x("/html/body/main/div[1]/div/div/div").shouldBe(visible);
+        $x("/html/body/main/div[1]/div/div/div/button/span").click();
+        $x("/html/body/main/div[1]/div/div/div").shouldNotBe(visible);
     }
 
 }
