989dd830277f52bbc0e5cc61a3c4773a73f7a756
Mikhail Shkaralevich <mkaralevich8@gmail.com>
2024-02-13 09:59:33 -1000

    Modify IdleLogoutTest

--- src/test/java/edu/hawaii/ti/iam/groupings/selenium/page/IdleLogoutTest.java
+++ src/test/java/edu/hawaii/ti/iam/groupings/selenium/page/IdleLogoutTest.java
@@ -1,55 +1,28 @@
 package edu.hawaii.ti.iam.groupings.selenium.page;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.FileDownloadMode;
-import com.codeborne.selenide.SelenideConfig;
-import com.codeborne.selenide.SelenideDriver;
-import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import com.opencsv.CSVWriter;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.File;
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.and;
 import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.cssClass;
-import static com.codeborne.selenide.Condition.disabled;
 import static com.codeborne.selenide.Condition.disappear;
-import static com.codeborne.selenide.Condition.empty;
-import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.exist;
-import static com.codeborne.selenide.Condition.interactable;
-import static com.codeborne.selenide.Condition.not;
-import static com.codeborne.selenide.Condition.selected;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.$$x;
-import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import edu.hawaii.ti.iam.groupings.selenium.core.User;
 
@@ -57,7 +30,6 @@ import edu.hawaii.ti.iam.groupings.selenium.core.User;
 public class IdleLogoutTest extends AbstractTestBase {
 
     private WebDriver driver;
-    private User admin;
     private User user;
 
     public IdleLogoutTest() {
@@ -83,11 +55,12 @@ public class IdleLogoutTest extends AbstractTestBase {
         user = createUser("student");
         loginWith(driver, user);
 
-        open(property.value("app.url.home") + "groupings");
+        open(property.value("app.url.home") + "/groupings");
         $(by("id", "overlay")).should(disappear, Duration.ofSeconds(80));
         $(byText("testiwte-many")).click();
         $(by("id", "sel")).shouldBe(visible);
     }
+    @Disabled
     @Test
     public void idleLogout() {
         Duration idleModal = Duration.ofSeconds(25 * 60 + 30);
